6cf4d862094c3a66f05cb696a2d70c09ed1f44d3
Mikhail Titov <mikhail.a.titov@gmail.com>
2025-06-27 17:42:08 -0400

    updated launcher script for BNL IC2

--- usecases/cell_painting_pipeline/launch/bnl_ic2_submit.sh
+++ usecases/cell_painting_pipeline/launch/bnl_ic2_submit.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=cellSAM      ## Name of the job
-#SBATCH --output=test_all_3_17_week2_new.out    ## Output file
+#SBATCH --output=cellSAM.out    ## Output file
 #SBATCH --partition csi
+#SBATCH -q csi
 #SBATCH --time=24:00:00         ## Job Duration
-#SBATCH --ntasks=1             ## Number of tasks (analyses) to run
-#SBATCH --cpus-per-task=8     ## The number of threads the code will use
-#SBATCH --gres=gpu:1            ## Real memory(MB) per CPU required by the job.
+#SBATCH --ntasks=6              ## Number of tasks (analyses) to run
+#SBATCH --cpus-per-task=8       ## The number of threads the code will use
+#SBATCH --gres=gpu:2            ## Real memory(MB) per CPU required by the job.
 #SBATCH -A csiml  
 
 
 export https_proxy=http://proxy.sdcc.bnl.local:3128/
-python test_all.py --data_dir '/hpcgpfs01/scratch/xyu1/cell_data/cellpaint/rpe_images/week_two/'
+# NOTE: activate virtual/conda environment first
+python3 cell.rp.py -c ic2.json
+# python test_all.py \
+#  --data_dir '/hpcgpfs01/scratch/xyu1/cell_data/cellpaint/rpe_images/week_two/'
