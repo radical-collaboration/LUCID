ddb7a7289bd37653f60ca84e854d44b44115dd2b
Mikhail Titov <mikhail.a.titov@gmail.com>
2025-12-12 10:09:12 -0500

    follow-up updates (AsyncFlow version)

--- usecases/cell_painting_pipeline/wfms/cell_asyncflow.py
+++ usecases/cell_painting_pipeline/wfms/cell_asyncflow.py
@@ -22,6 +22,7 @@ os.environ['RADICAL_REPORT'] = 'TRUE'
 
 logger = logging.getLogger(__name__)
 
+
 def filter_input_images(images_dir: str, base_channel: str) -> list:
     output = []
     for image_path in glob.glob(f'{images_dir}/*'):
@@ -34,6 +35,7 @@ def filter_input_images(images_dir: str, base_channel: str) -> list:
 def datetime_now():
     return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+
 class Pipeline:
 
     def __init__(self,
@@ -45,10 +47,13 @@ class Pipeline:
                  output_dir: Optional[str] = None):
 
         self.flow = flow
-        self.images = []
         self.name = name
-        self.cfg = config['pipeline_cfg']
+        self.logger = logger
 
+        self.cfg = config['pipeline_cfg']
+        self.cfg_exec = config['exec_cfg']
+
+        self.images = []
         if image_path:
             self.images.append(image_path)
         elif input_dir and os.path.isdir(input_dir):
@@ -60,60 +65,63 @@ class Pipeline:
                           os.path.dirname(self.images[0])
         os.makedirs(self.output_dir, exist_ok=True)
 
-        self.stage_id = 0
+        self.register_pipeline_tasks()
 
-    
     def register_pipeline_tasks(self):
         """Register all pipeline tasks"""
 
-        s1_task_desc = {"ranks": 1, "named_env": "rp",
-                        "gpus_per_rank": 1,
-                        "pre_exec": self.cfg['task_pre_exec'] or [],
-                        "environment":self.cfg['task_environment'] or {}}
+        s1_task_desc = {'ranks': 1,
+                        'named_env': 'rp',
+                        'gpus_per_rank': 1,
+                        'pre_exec': self.cfg_exec['task_pre_exec'] or [],
+                        'environment': self.cfg_exec.get('task_environment', {})}
 
-        s2_task_desc = {"ranks": 1, "named_env": "rp",
-                        "pre_exec": self.cfg['task_pre_exec'] or [],
-                        "environment":self.cfg['task_environment'] or {}}
-
+        s2_task_desc = {'ranks': 1,
+                        'named_env': 'rp',
+                        'pre_exec': self.cfg_exec['task_pre_exec'] or [],
+                        'environment': self.cfg_exec.get('task_environment', {})}
 
         @self.flow.executable_task
-        async def stage_1(image_path: str, task_description: dict = s1_task_desc):  # noqa: B006
+        async def stage_1(image_path: str,
+                          task_description: dict = s1_task_desc):  # noqa: B006
             """Segmentation."""
             run_script = 'cell_segmentation_complete.py'
             arguments = ['--base_channel', self.cfg['base_channel'],
-                        '--target_channel', self.cfg['target_channel'],
-                        '--bbox_threshold', self.cfg['bbox_threshold'],
-                        '--output_dir', self.cfg['output_dir'],
-                        '--model_path', self.cfg['model_path']
+                         '--target_channel', self.cfg['target_channel'],
+                         '--bbox_threshold', self.cfg['bbox_threshold'],
+                         '--output_dir', self.cfg['output_dir'],
+                         '--model_path', self.cfg['model_path']]
             if self.cfg['save_bbox']:
                 arguments += ['--save_bbox']
 
-            cmd = f"python '$RP_PILOT_SANDBOX/{run_script} --image_path' {image_path}"
-            cmd += ' '.join(arguments)
-
-            return cmd
+            return (f'python $RP_PILOT_SANDBOX/{run_script} '
+                    + f'--image_path {image_path} '
+                    + ' '.join([str(a) for a in arguments]))
 
         @self.flow.executable_task
-        def stage_2(task_description=s2_task_desc):   # noqa: B006
+        async def stage_2(task_description: dict = s2_task_desc):  # noqa: B006
             """Analysis."""
 
             run_script = 'segmentation_analysis.py'
             arguments = ['--images_dir', self.output_dir,
-                        '--base_channel', self.cfg['base_channel'],
-                        '--target_channel', self.cfg['target_channel'],
-                        '--week_name', self.cfg['week_name'],
-                        '--plate_name', self.cfg['plate_name'],
-                        '--plate_config', self.cfg['plate_config'],
-                        '--feature_type', self.cfg['feature_type']]
+                         '--base_channel', self.cfg['base_channel'],
+                         '--target_channel', self.cfg['target_channel'],
+                         '--week_name', self.cfg['week_name'],
+                         '--plate_name', self.cfg['plate_name'],
+                         '--plate_config', self.cfg['plate_config'],
+                         '--feature_type', self.cfg['feature_type']]
 
-            cmd = f"python $RP_PILOT_SANDBOX/{run_script}"
-            cmd += ' '.join(arguments)
+            return (f'python $RP_PILOT_SANDBOX/{run_script} '
+                    + ' '.join([str(a) for a in arguments]))
 
+        setattr(self, 'stage_1', stage_1)
+        setattr(self, 'stage_2', stage_2)
 
     async def run(self):
         """Main execution logic"""
 
-        self.logger.pipeline_log(f"Pipeline {self.name} started")
+        self.logger.info(f'Pipeline {self.name} started '
+                         f'with {len(self.images)} images')
 
         stage_1_submitted_tasks = []
 
@@ -128,7 +136,6 @@ class Pipeline:
         results = await stage_2_future
 
         print(results)
-
 
 
 # ------------------------------------------------------------------------------
@@ -178,16 +185,20 @@ async def main():
     config_file = args.config_file
     if '/' not in config_file:
         config_file = os.path.join(WFMS_DIR, config_file)
-
     with open(config_file, 'r') as f:
         config = json.load(f)
-
     if not len(config):
         raise ValueError(f'Config is empty (file: {config_file})')
 
     # adjust runtime
+    run_description = dict(**config['run_description'],
+                           input_staging=glob.glob(f'{SRC_DIR}/*'),
+                           sandbox=os.path.abspath(args.work_dir or WFMS_DIR))
     if args.runtime:
-        config['run_description']['runtime'] = int(args.runtime)
+        run_description['runtime'] = int(args.runtime)
+
+    backend = await RadicalExecutionBackend(run_description)
+    flow = await WorkflowEngine.create(backend=backend)
 
     input_dir = args.input_dir or config['pipeline_cfg']['input_dir']
     # NOTE: create a pipeline per directory with images, thus stage2 will be
@@ -196,12 +207,6 @@ async def main():
     input_dirs = input_dirs or [input_dir]
 
     pipes = []
-
-    print(f'{datetime_now()} Processing {p.name} has started with ')
-
-    backend = await RadicalExecutionBackend({'nodes': 1, 'resources': 'local.localhost'})
-    flow = await WorkflowEngine.create(backend=backend)
-
     for pipe_id, input_dir in enumerate(input_dirs):
         p = Pipeline(name=f'pipe-{pipe_id}',
                      config=config,
@@ -211,7 +216,13 @@ async def main():
 
         p_task = p.run()
         pipes.append(p_task)
+        print(f'{datetime_now()} Processing {p.name} has started')
 
     results = await asyncio.gather(*pipes)
 
     print(f'All pipeline results: {results}')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
